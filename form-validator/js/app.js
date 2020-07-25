997d625ba23ec110c58ab904a443e2b459f8baa6
marcin_ptok <marcin111100@gmail.com>
2020-07-25 23:44:40 +0200

    Completed validation

--- form-validator/js/app.js
+++ form-validator/js/app.js
@@ -2,6 +2,10 @@ const sliderContainer = document.querySelector('.login-slider');
 const sliderItems = document.querySelectorAll('.login-slider li');
 const sliderElement = document.querySelector('.focus-el');
 const formsContainer = document.querySelector('.forms-container');
+const register_form = document.querySelector('.form-signup-container');
+const login_form = document.querySelector('.form-login-container');
+
+/* Forms slider */
 let sliderContainerRect = sliderContainer.getBoundingClientRect();
 
 let activeIndex = 0;
@@ -43,4 +47,171 @@ sliderItems.forEach((li, index) => {
         break;
     }
   });
+});
+
+/* Register form submit */
+
+const getFieldName = (input) => {
+  let name = input.getAttribute('name');
+  name = name.replace('_', ' ');
+  return name.charAt(0).toUpperCase() + name.slice(1);
+};
+
+const showError = (input, msg) => {
+  const parent = input.parentElement;
+  const errorContainer = parent.querySelector('.error-msg');
+  parent.classList.add('error');
+  errorContainer.textContent = msg;
+  errorContainer.style.visibility = 'visible';
+};
+
+const showSuccess = (input) => {
+  const parent = input.parentElement;
+  if (parent.classList.contains('error')) {
+    parent.classList.remove('error');
+  }
+  parent.classList.add('success');
+  const errorContainer = parent.querySelector('.error-msg');
+  errorContainer.style.visibility = 'hidden';
+};
+
+const checkRequired = (inputs) => {
+  let errorFlag = false;
+  inputs.forEach((input) => {
+    if (input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+      errorFlag = true;
+    } else {
+      showSuccess(input);
+    }
+  });
+  return errorFlag;
+};
+
+const checkLength = (input, min, max) => {
+  let errorFlag = false;
+  const valueLength = input.value.length;
+  if (valueLength < min) {
+    showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    errorFlag = true;
+  } else if (valueLength > max) {
+    showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    errorFlag = true;
+  } else {
+    showSuccess(input);
+  }
+  return errorFlag;
+};
+
+const checkEmail = (input) => {
+  let errorFlag = false;
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (regEx.test(String(input.value).toLowerCase())) {
+    showSuccess(input);
+  } else {
+    showError(input, `${getFieldName(input)} is not valid`);
+    errorFlag = true;
+  }
+  return errorFlag;
+};
+
+const checkPasswordStrong = (input) => {
+  let errorFlag = false;
+  const regEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})/;
+  if (regEx.test(input.value)) {
+    showSuccess(input);
+  } else {
+    showError(input, `$Password is not strong`);
+    errorFlag = true;
+  }
+  return errorFlag;
+};
+
+const checkMatch = (input, input2) => {
+  let errorFlag = false;
+  if (input.value === input2.value) {
+    showSuccess(input2);
+  } else {
+    showError(input, `Passwords do not match`);
+    errorFlag = true;
+  }
+  return errorFlag;
+};
+
+const checkConfirm = (input) => {
+  let errorFlag = false;
+  if (input.checked) {
+    const parent = input.parentElement;
+    if (parent.classList.contains('error')) {
+      parent.classList.remove('error');
+    }
+    parent.classList.add('success');
+  } else {
+    const parent = input.parentElement;
+    parent.classList.add('error');
+  }
+  return errorFlag;
+};
+
+register_form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const firstname = register_form.firstname;
+  const lastname = register_form.lastname;
+  const username = register_form.username;
+  const email = register_form.email;
+  const password = register_form.password;
+  const confirm_password = register_form.confirm_password;
+  const confirm = register_form.confirm;
+
+  let errorFlag = false;
+  /* check if errors */
+  if (checkRequired([firstname, lastname, username, email, password, confirm_password])) {
+    errorFlag = true;
+  }
+
+  if (checkLength(username, 4, 16)) {
+    errorFlag = true;
+  }
+
+  if (checkEmail(email)) {
+    errorFlag = true;
+  }
+
+  if (checkPasswordStrong(password)) {
+    errorFlag = true;
+  }
+
+  if (checkMatch(password, confirm_password)) {
+    errorFlag = true;
+  }
+
+  if (checkConfirm(confirm)) {
+    errorFlag = false;
+  }
+
+  if (errorFlag) {
+    console.error('Błąd!');
+  } else {
+    //TODO: Add better information about signup
+    const user = {
+      username: username.value,
+      password: password.value,
+    };
+
+    alert('zalogowano');
+    localStorage.setItem('user', JSON.stringify(user));
+    console.log({
+      username: username.value,
+      password: password.value,
+    });
+
+    animateForm('-50% - 10px ', '0px');
+    //TODO: add function to move top slider
+  }
+});
+
+login_form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  //TODO: Add login
 });
